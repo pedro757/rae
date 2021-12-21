2c35cf990a16bfdca46677f87e2965d847dd4caf
Pedro Sanchez <pedrosanchez757@hotmail.com>
2021-12-21 17:59:03 -0400

    refactor: refactor for linters

--- rae/__init__.py
+++ rae/__init__.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.by import By
 from flask import Flask, jsonify
 from flask_restful import Api, Resource
 import pandas as pd
+import numpy
 
 app = Flask(__name__)
 api = Api(app)
@@ -14,11 +15,12 @@ options.headless = True
 
 class words_bulk(Resource):
   def get(self):
-    words = pd.read_csv('palabras.csv', names=['word', 'meaning', 'examples', 'url'],
+    columns = numpy.array(["word", "meaning", "examples", "url"])
+    words = pd.read_csv('palabras.csv', names=columns,
       dtype={'word': 'str', 'meaning': 'str', 'examples': 'str', 'url': 'str' })
     browser = webdriver.Firefox(options=options)
     for row in words.itertuples():
-      url = "https://dle.rae.es/" + row.word
+      url = "https://dle.rae.es/" + getattr(row, "word")
       browser.get(url)
       lines = browser.find_elements(By.XPATH,
         "//div[@id='resultados']/article/p[not(@class='n5') and not(starts-with(@class, 'l'))]")
@@ -34,11 +36,11 @@ class words_bulk(Resource):
         if line.text[0].isnumeric():
           breakline = "\n" if line.text != lines[-1].text else ""
           meaning = meaning + line.text + breakline
-      words.at[row.Index, 'meaning'] = meaning
+      words.at[getattr(row, "Index"), 'meaning'] = meaning
       for ex in examples:
         eg = eg + ex.text + "\n"
-      words.at[row.Index, 'examples'] = eg
-      words.at[row.Index, 'url'] = url
+      words.at[getattr(row, "Index"), 'examples'] = eg
+      words.at[getattr(row, "Index"), 'url'] = url
     browser.close()
     words.to_csv('definiciones.csv', header=False, index=False, line_terminator='←', sep='↓')
     return jsonify('ok')
