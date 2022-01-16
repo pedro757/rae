5b120a21886b1c03319b9ecd43ee5894fae16932
Pedro Sanchez <pedrosanchez757@hotmail.com>
2022-01-16 11:22:38 -0400

    test: check exact definition

--- tests/test_rae.py
+++ tests/test_rae.py
@@ -9,5 +9,11 @@ def palabra_menester():
 def test_acepciones_not_empty(palabra_menester):
   assert len(palabra_menester[0]['acepciones']) >= 1
 
+def test_definition(palabra_menester):
+  acepciones = palabra_menester[0]['acepciones']
+  assert acepciones[0] == "1. m. Falta o necesidad de algo."
+  assert acepciones[1] == "2. m. Oficio u ocupación habitual U. m. en pl."
+  assert acepciones[2] == "3. m. pl. Necesidades fisiológicas."
+
 def test_ethymology(palabra_menester):
   assert palabra_menester[0]['etimologia'] == "Del lat. ministerium 'servicio', 'oficio'."
