d798eff8bdc2f07eeecbfaf2ed73b5e0e5f12a18
Pedro Sanchez <pedrosanchez757@hotmail.com>
2022-01-15 21:47:22 -0400

    fix: send etimology get_definition

--- rae/rae.py
+++ rae/rae.py
@@ -1,6 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
+from typing import TypedDict
+
+class Raiz(TypedDict):
+  etimologia: str
+  acepciones: list[str]
 
 class Definitions():
   def __init__(self):
@@ -9,18 +14,27 @@ class Definitions():
     self.browser = webdriver.Firefox(options=options)
     self.url = "https://dle.rae.es/"
 
-  def get_definition(self, word:str) -> dict[str, list[str]]:
-    self.browser.get(self.url + word)
-    definition = self.browser.find_elements(By.XPATH,
-      "//div[@id='resultados']/article/p[not(@class='n5') and not(starts-with(@class, 'l'))]")
-    phrases = self.browser.find_elements(By.XPATH,
-      "//div[@id='resultados']/article/p[starts-with(@class, 'l')]")
+  def get_definition(self, word:str) -> list[Raiz]:
+    palabra:list[Raiz] = []
     acepciones = []
-    frases = []
-    for acepcion in definition:
-      acepciones.append(acepcion.text)
-    for phrase in phrases:
-      frases.append(phrase)
+    locuciones = []
+    self.browser.get(self.url + word)
+    raices = self.browser.find_elements(By.XPATH,
+      "//div[@id='resultados']/article")
+    for raiz in raices:
+      id = raiz.get_attribute('id')
+      article = f"//article[@id='{id}']/"
+      etimologia = self.browser.find_element(By.XPATH,
+        article + "p[starts-with(@class, 'n2')]")
+      acepciones = self.browser.find_elements(By.XPATH,
+        article + "p[starts-with(@class, 'j')]")
+      palabra.append({
+        "etimologia": etimologia.text,
+        "acepciones": [acepcion.text for acepcion in acepciones],
+      })
+    self.browser.close()
+    return palabra
+
     self.browser.close()
     return {
       "acepciones": acepciones,
